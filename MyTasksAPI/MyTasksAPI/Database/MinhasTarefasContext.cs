124385b6449cf2f519ccf3c2046b5b9457b22ead
Guilherme Taveira <guibalestra91@gmail.com>
2021-01-15 18:07:46 -0300

    Criando e configurando DbContext

---
+++ MyTasksAPI/MyTasksAPI/Database/MinhasTarefasContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyTasksAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTasksAPI.Database
+{
+    public class MinhasTarefasContext : DbContext
+    {
+        public MinhasTarefasContext(DbContextOptions<MinhasTarefasContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+}
