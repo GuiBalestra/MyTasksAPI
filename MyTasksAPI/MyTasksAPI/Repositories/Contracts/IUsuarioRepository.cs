f53011cb1bff48024c7853d996f3dbd4e5d73fbc
Guilherme Taveira <guibalestra91@gmail.com>
2021-01-18 19:22:31 -0300

    Repository e Interface

---
+++ MyTasksAPI/MyTasksAPI/Repositories/Contracts/IUsuarioRepository.cs
@@ -0,0 +1,15 @@
+﻿using MyTasksAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTasksAPI.Repositories.Contracts
+{
+    public interface IUsuarioRepository
+    {
+        void Cadastrar(ApplicationUser usuario, string senha);
+
+        ApplicationUser Obter(String email, string senha);
+    }
+}
