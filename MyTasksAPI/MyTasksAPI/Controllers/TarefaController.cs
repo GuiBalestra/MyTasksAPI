51b416be4bc30c5b0f7b04b8b9c60040bcac1f1c
Guilherme Taveira <guibalestra91@gmail.com>
2021-01-19 17:58:12 -0300

    TarefaController Métodos Sincronizar e Restaurar

---
+++ MyTasksAPI/MyTasksAPI/Controllers/TarefaController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyTasksAPI.Models;
+using MyTasksAPI.Repositories.Contracts;
+
+namespace MyTasksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarefaController : ControllerBase
+    {
+        private readonly ITarefaRepository _tarefaRepository;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public TarefaController(ITarefaRepository tarefaRepository, UserManager<ApplicationUser> userManager)
+        {
+            _tarefaRepository = tarefaRepository;
+            _userManager = userManager;
+        }
+
+        public ActionResult Sincronizar([FromBody] List<Tarefa> tarefas)
+        {
+            return Ok(_tarefaRepository.Sincronizacao(tarefas));
+        }
+
+        public ActionResult Restaurar(DateTime data)
+        {
+            var usuario = _userManager.GetUserAsync(HttpContext.User).Result;
+
+            return Ok(_tarefaRepository.Restauracao(usuario, data));
+        }
+    }
+}
