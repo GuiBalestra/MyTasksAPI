f8eec1670245268f9a468b0d63546dc621b3b8d5
Guilherme Taveira <guibalestra91@gmail.com>
2021-01-19 17:30:31 -0300

    Método Cadastrar na UsuarioController

--- MyTasksAPI/MyTasksAPI/Controllers/UsuarioController.cs
+++ MyTasksAPI/MyTasksAPI/Controllers/UsuarioController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -16,11 +17,13 @@ namespace MyTasksAPI.Controllers
     {
         private readonly IUsuarioRepository _usuarioRepository;
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public UsuarioController(IUsuarioRepository usuarioRepository, SignInManager<ApplicationUser> signInManager)
+        public UsuarioController(IUsuarioRepository usuarioRepository, SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
         {
             _usuarioRepository = usuarioRepository;
             _signInManager = signInManager;
+            _userManager = userManager;
         }
 
         public ActionResult Login([FromBody] UsuarioDTO usuarioDTO)
@@ -42,6 +45,36 @@ namespace MyTasksAPI.Controllers
                 else
                 {
                     return NotFound("Usuário não localizado!");
+                }
+            }
+            else
+            {
+                return UnprocessableEntity(ModelState);
+            }
+        }
+
+        public ActionResult Cadastrar([FromBody] UsuarioDTO usuarioDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser usuario = new ApplicationUser();
+                usuario.FullName = usuarioDTO.Nome;
+                usuario.Email = usuarioDTO.Email;
+
+                var resultado = _userManager.CreateAsync(usuario, usuarioDTO.Senha).Result;
+
+                if (!resultado.Succeeded)
+                {
+                    List<string> erros = new List<string>();
+                    foreach (var erro in resultado.Errors)
+                    {
+                        erros.Add(erro.Description);
+                    }
+                    return UnprocessableEntity(erros);
+                }
+                else
+                {
+                    return Ok(usuario);
                 }
             }
             else
